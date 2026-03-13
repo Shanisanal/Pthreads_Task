d60108891b04788ec0dc04c655d9d6b231fe27da
Shanisanal <shani.sanal@trenser.com>
2026-03-13 07:02:54 +0000

    Added ThreadHandler header file for Thread Handler fucntion declarations

---
+++ Include/ThreadHandler.h
@@ -0,0 +1,32 @@
+//************************** Pthreads_Task  ***********************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : ThreadHandler.h
+// Summary  : 
+// Note     : 
+// Author   : Shani 
+// Date     : Mar 12, 2026 
+//
+//*****************************************************************************
+#ifndef THREADHANDLER_H
+#define THREADHANDLER_H
+
+//******************************* Include Files ******************************* 
+
+//******************************* Global Types ******************************** 
+ 
+//***************************** Global Constants ****************************** 
+
+//***************************** Global Variables ****************************** 
+
+//**************************** Forward Declarations *************************** 
+bool ExecuteThreads(void);
+bool CreateMessageQueue (void)
+
+//*********************** Inline Method Implementations *********************** 
+
+#endif  /*THREADHANDLER_H*/
+
+// EOF 
